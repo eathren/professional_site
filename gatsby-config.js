132411f271b58f18480e948bef63bb61920f3bf4
eathren <nolan.braman@gmail.com>
2021-03-02 20:13:24 -0500

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Nolan Braman`,
+    description: `Software Engineer in the Greater Boston Area.`,
+    author: `@eathren`,
+    social: {
+      github: `https://github.com/eathren`,
+      linkedin: `https://www.linkedin.com/in/nolanbraman/`,
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -17,20 +21,26 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography/typography.js`,
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Nolan Braman | Software Engineer`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#007ea78p`,
+        theme_color: `#007ea78p`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/android-chrome-512x512.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
